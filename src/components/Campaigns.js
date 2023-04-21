3bca146bf6a72db0d6198e28a2d23bc5c7d307c9
tugrankenger <turankenger@hotmail.com.tr>
2023-04-21 12:22:13 +0300

    complete campaigns section

--- src/components/Campaigns.js
+++ src/components/Campaigns.js
@@ -1,5 +1,39 @@
+import { useState, useEffect } from 'react'
+import Banners from 'api/banners.json'
+import Slider from 'react-slick';
+import Title from './ui/Title';
+
 export default function Campaigns(){
+  const [banners, setBanners] = useState([])
+
+  useEffect(()=>{
+    setBanners(Banners)
+  },[])
+
+  const settings = {
+    dots: false,
+    infinite: true,
+    speed: 500,
+    slidesToShow: 3,
+    slidesToScroll: 1,
+    arrows: false,
+    autoplay: true,
+    autoplaySpeed: 2500,
+    cssEase: "linear"
+  };
+
   return(
-    <div>Campaigns</div>
+    <div className='container mx-auto py-8'>
+      <Title>Kampanyalar</Title>
+      <Slider className='-mx-2' {...settings}>
+      {banners.length && banners.map((banner,index)=>(
+        <div key={banner.id}>
+          <picture className='block px-2'>
+            <img src={banner.image} className='rounded-lg' />
+          </picture>
+        </div>
+      ))}
+      </Slider>
+    </div>
   )
 }
---
+++ src/components/ui/Title.js
@@ -0,0 +1,6 @@
+
+export default function Title({children}){
+  return(
+    <h3 className='text-sm font-semibold mb-3'>{children}</h3>
+  )
+}
